d49e759a2333b8a4b02eaa0fbf3c56da7feb4191
tsperr <t.sperr22@gmail.com>
2021-07-17 10:23:52 -0400

    flask

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class JSONEncoder(json.JSONEncoder):
         if isinstance(o, ObjectId):
             return str(o)
         return json.JSONEncoder.default(self, o)
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/project_2"
 app.json_encoder=JSONEncoder
@@ -23,20 +23,24 @@ mongo = PyMongo(app)
 
    # Client sends their string, we interpret it as an ObjectId
 @app.route('/data')
-def home():
+def data():
     
     movie_data=mongo.db.IMDBdata.find()
     json_str = dumps(movie_data)
     record2 = loads(json_str)
     
-    #return render_template("index.html", listings=record2)
     return jsonify(record2)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 @app.route('/ratings')
 def ratings_graphs():
     
     
-    return render_template("index.html")
+    return render_template("templates/\html/RatingsRevenue.html")
 
 
 if __name__ == '__main__':    
--- static/js/ratingsrevenue.js
+++ static/js/ratingsrevenue.js
@@ -1,2 +1,3 @@
 d3.json("/data").then(data=> console.log(data)).catch(error => console.log(error))
 
+
