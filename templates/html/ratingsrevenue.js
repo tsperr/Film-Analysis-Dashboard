008ef975ea0e03bdfcb71b4f28374bb21bfba2b1
hvinoy <vinoyharsha@gmail.com>
2021-07-17 13:42:14 -0400

    json file

--- templates/html/ratingsrevenue.js
+++ templates/html/ratingsrevenue.js
@@ -1,3 +1,12 @@
+d3.json("../../movie_data.json", callback)
+    function callback(data) {
+        // console.log(data)
+        console.log(data[budget])
+    };
+
+
+
+
 // d3.json("/ratings").then(data=> console.log(data)).catch(error => console.log(error))
 // function callback(data) {
 //     console.log(data)
@@ -13,40 +22,40 @@
 
 
 
-function init() {
+// function init() {
 
-    d3.json("http://localhost:5000/data", callback) 
-    function callback(data) {
-        console.log(data)
-        var budget = [];
-        var revenue = [];
+//     d3.json("http://localhost:5000/data", callback) 
+//     function callback(data) {
+//         console.log(data)
+//         var budget = [];
+//         var revenue = [];
 
-        for (var i = 0; i<data.length; i++) {
-            item = data[i]["budget"]
-            budget.push(item)
-            item2 = data[i]["worlwide_gross_income"]
-            revenue.push(item2)
-        }
-    //     data.forEach(function(row) {
-    //       console.log(row)
+//         for (var i = 0; i<data.length; i++) {
+//             item = data[i]["budget"]
+//             budget.push(item)
+//             item2 = data[i]["worlwide_gross_income"]
+//             revenue.push(item2)
+//         }
+//     //     data.forEach(function(row) {
+//     //       console.log(row)
         
-    //     budget.push(row["budget"]);
-    //     console.log(budget)  
+//     //     budget.push(row["budget"]);
+//     //     console.log(budget)  
         
-    //     revenue.push(row["worlwide_gross_income"]);
-    //   });
-      ///scatter plot budget vs income
-      var trace1 = {
-          x: budget,
-          y: revenue,
-          mode:"markers",
-          type: "scatter"
-      };
-      var data = [trace1];
-      Plotly.newPlot("budgvsrev",data);
-    }
-}
-init()
+//     //     revenue.push(row["worlwide_gross_income"]);
+//     //   });
+//       ///scatter plot budget vs income
+//       var trace1 = {
+//           x: budget,
+//           y: revenue,
+//           mode:"markers",
+//           type: "scatter"
+//       };
+//       var data = [trace1];
+//       Plotly.newPlot("budgvsrev",data);
+//     }
+// }
+// init()
 
 
 
