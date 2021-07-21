29119f16afda591626e41af8d81005f2f58b6ad5
BigDataEnergy <76790142+BigDataEnergy@users.noreply.github.com>
2021-07-20 21:49:13 -0400

    commit

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
+import numpy as np
+import os
 from flask_pymongo import PyMongo
+from flask.json import JSONEncoder
+from bson.json_util import loads, dumps
+from bson import ObjectId 
+import json
+from flask_cors import CORS
 import top_tens
 
-app = Flask(__name__)
-
-# Use flask_pymongo to set up mongo connection
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+app = Flask(__name__, template_folder='templates')
+CORS(app,support_credentials=True)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/project_2"
+app.json_encoder=JSONEncoder
 mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/phone_app")
-
 
 @app.route("/")
 def index():
     top_tens.scrape()
     top_ten = mongo.db.top_ten.find()
-    print(top_ten)
     return render_template("index.html", top_ten=top_ten)
 
-
-#@app.route("/null")
-def scraper():
-    top_ten = mongo.db.top_ten
-    top_data = top_tens.scrape()
-    return redirect("/", code=302)
-
+@app.route("/top")
+def data():
+    top_tens.scrape()
+    top_ten_data = mongo.db.top_ten.find()
+    json_str = dumps(top_ten_data)
+    record2 = loads(json_str)
+    print(record2)
+    return jsonify(record2)
+     
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/t10.js
@@ -0,0 +1,23 @@
+d3.json("http://localhost:5000/top").then((data) => {
+  
+    console.log(data);
+     var titles = [];
+     var grosses = []
+     for (var i = 0; i<data.length; i++) {
+          title = data[i].title
+          titles.push(title)
+           gross = data[i].totalGross
+           grosses.push(gross);
+   };
+    console.log(titles);
+    console.log(grosses);
+
+    var data = [{
+      type: 'bar',
+      x: titles,
+      y: grosses,
+      orientation: 'v'
+    }];
+    Plotly.newPlot('bar', data);
+
+});
